ab96c6f7d23f3b264317579d598b408dccdc5057
felsec01 <felipesecundino@gmail.com>
2026-03-03 18:10:57 -0300

    Update Mercadopago public key in integration config

--- config-integracao.js
+++ config-integracao.js
@@ -7,9 +7,10 @@ window.CLEAN_HELMET_INTEGRACAO = {
   security: window.CLEAN_HELMET_CONFIG.security,
   deviceSystem: window.CLEAN_HELMET_CONFIG.deviceSystem,
   mercadopago: {
-    publicKey: "TEST-xxxxxxxxxxxxxxxxxxxxxx", // substitua pela sua chave pública
+    publicKey: "TEST-336e3af6-b446-444e-9fc8-cfb46d8ef480", // substitua pela sua chave pública
     sandboxMode: window.CLEAN_HELMET_ENV.isDevelopment
   }
 };
 
 console.log("🔧 Configuração de integração carregada:", window.CLEAN_HELMET_INTEGRACAO);
+
