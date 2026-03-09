2f2dd5d722aabf1844fa342b5dd19398fedc455d
felsec01 <felipesecundino@gmail.com>
2026-03-08 21:14:53 -0300

    Update firebase-admin.js

--- firebase-admin.js
+++ firebase-admin.js
@@ -1,12 +1,16 @@
 // firebase-admin.js
 const admin = require("firebase-admin");
-
-// Carrega credenciais do Firebase via variável de ambiente
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://cleanhelmet-e55b7-default-rtdb.firebaseio.com"
-});
+if (!admin.apps.length) { // evita inicialização duplicada
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://cleanhelmet-e55b7-default-rtdb.firebaseio.com"
+  });
+
+  console.log("✅ Firebase Admin inicializado com projeto:", serviceAccount.project_id);
+} else {
+  console.log("⚠️ Firebase Admin já estava inicializado, reutilizando instância existente");
+}
 
 module.exports = admin;
